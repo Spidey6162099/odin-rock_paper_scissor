280a189a6955f96cb3ef646c8829ae4c15302f6a
Shaunak <141052710+Spidey6162099@users.noreply.github.com>
2024-01-17 23:21:09 +0530

    [feature] added ui for 3 round rock_paper_scissor [desgin] barebone done

--- javascript.js
+++ javascript.js
@@ -1,23 +1,23 @@
 const arr=["rock","scissors","paper"];
-
-
-
+let player_score=0,computer_score=0,count=0;
+const result=document.querySelector("#score_card>#results");
+result.setAttribute('style','color:purple;font-size:60px')
 function getComputerchoice(){
 let rando=Math.floor(Math.random()*3);
 return arr[rando];
 }
 
-function getResult(choice){
- if(choice==""){
-    return 3;
- }
- else if(choice=="scissors"){
-    return 2;
- }
- else{
-    return 1;
- }
-}
+// function getResult(choice){
+//  if(choice==""){
+//     return 3;
+//  }
+//  else if(choice=="scissors"){
+//     return 2;
+//  }
+//  else{
+//     return 1;
+//  }
+// }
 
 function play_one_round(computerChoice,playerChoice){
     if(computerChoice=="rock"){
@@ -33,46 +33,72 @@ function play_one_round(computerChoice,playerChoice){
         
 }
 
-function the_game(){
-    let count=0;
-    let player_score=0,computer_score=0;
-    while(count<3){
-    let playerChoice=(prompt("enter one of the following options: rock,paper,scissor","rock")).toLowerCase();
+function the_game(playerChoice){
+    result.textContent="";
+    const play_score=document.querySelector("#score_card>#scores_player");
+    const comp_score=document.querySelector("#score_card>#scores_computer");
+   
+    //let playerChoice=(prompt("enter one of the following options: rock,paper,scissor","rock")).toLowerCase();
     let computerChoice=getComputerchoice();
-    console.log(computerChoice);
-    console.log(playerChoice);
-    if(computerChoice==playerChoice){
-        console.log("tis a draw");
-        continue;
-    }
+    // console.log(computerChoice);
+    // console.log(playerChoice);
+    const round=document.querySelector("#rounds");
+    round.textContent=`current round:${count+1}`;
     console.log(`the player has chosen ${playerChoice} while computer ${computerChoice}`);
+        if(computerChoice==playerChoice){
+        console.log("draw");
+        
+    }
+    
     if(play_one_round(computerChoice,playerChoice)){
 
         computer_score++;
-        console.log("the computer wins this round");
+        //console.log("the computer wins this round");
+       
     }
     else{
         player_score++;
-        console.log("ha the ai overlord is crushed this round");
+        //console.log("ha the ai overlord is crushed this round");
     }
     count++;
-    }
-    if(player_score>computer_score){
-        console.log("tis a victory for rebellion");
-    }
-    else{
-        console.log("alas laddie,defeat it is the skynet prevails");
-    }
-
-    
-    let ch=prompt("do you wish to have another go y/n?");
-    if(ch=="y"){
-        the_game();
-    }
-    else{
-        console.log("thank you");
-    }
-    
-
+    play_score.textContent=`player: ${player_score}`;
+    comp_score.textContent=`computer: ${computer_score}`;
+    if(count ==3){
+        
+         
+         if(player_score>computer_score){
+         result.textContent="victory";
+        }
+        else if(player_score<computer_score){
+            result.textContent="loss";
+        }
+        else{
+            result.textContent="tie";
+        }
+        count=0;
+        player_score=0;
+        computer_score=0;
+        
+    }   
 }
-the_game();
+const rock=document.querySelector("div#choice_container>#rock");
+
+const scissor=document.querySelector("div#choice_container>#scissor");
+const paper=document.querySelector("div#choice_container>#paper");
+
+rock.addEventListener('mousedown',(e)=>{
+    
+    if(e.button==0){
+    the_game("rock")}
+
+});
+scissor.addEventListener('mousedown',(e)=>{
+    if(e.button==0){
+    the_game("scissors")}
+
+});
+paper.addEventListener('mousedown',(e)=>{
+    if(e.button==0){
+    the_game("paper")}
+
+});
